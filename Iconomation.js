116a279b6a59ac20877dae26aa778ad8cd9ed053
Astrolingus <ahuramazda.ahriman@gmail.com>
2014-09-24 23:26:21 -0400

    Create Iconomation.js

---
+++ Iconomation.js
@@ -0,0 +1,37 @@
+// add icons
+$('.playspace').append('<i class="em em-person_with_blond_hair"></i>');
+$('.playspace').append('<i class="em em-woman"></i>');
+$('.playspace').append('<i class="em em-baby"></i>');
+$('.playspace').append('<i class="em em-necktie"></i>');
+$('.playspace').append('<i class="em em-dress"></i>');
+
+// set the initial positions
+
+$('.em-person_with_blond_hair').css({
+  top: 200,
+  left: 300
+});
+
+$('.em-woman').css({
+  top: 210,
+  left: 100
+});
+
+
+// play!
+
+$('.em-person_with_blond_hair').animate({
+  left: 50
+}, 1000);
+
+$('.woman').animate({
+  left: 50
+}, 1000);
+
+// after 660ms...
+//setTimeout(function() {
+  // ...move the ball
+  //$('.fa-futbol-o').animate({
+    //left: 0
+  //}, 800, 'easeOutQuart');
+//}, 660);
