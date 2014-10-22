36f08137cabb4a39a34e68acefedfaa6b0ab01c8
Astrolingus <ahuramazda.ahriman@gmail.com>
2014-10-22 10:49:45 -0400

    clicking works hurrah!

--- Is_He_Gay.js
+++ Is_He_Gay.js
@@ -1,33 +1,37 @@
 var soundButton, hiddenSound;
 
+
+function init()
+{
+
+  // show and hide the popup
+  $('#sound').click(function() { $('#popUpSound').show(); });
+  $('#popUpSound').click(function() { $('#popUpSound').hide(); });
+
+  $('#look').click(function() { $('#popUpLook').show(); });
+  $('#popUpLook').click(function() { $('#popUpLook').hide(); });
+
+  $('#interests').click(function() { $('#popUpInterests').show(); });
+  $('#popUpInterests').click(function() { $('#popUpInterests').hide(); });
+
+  $('#girl').click(function() { $('#popUpGirl').show(); });
+  $('#popUpGirl').click(function() { $('#popUpGirl').hide(); });
+
+  $('#nice').click(function() { $('#popUpNice').show(); });
+  $('#popUpNice').click(function() { $('#popUpNice').hide(); });
+
+  $('#men').click(function() { $('#popUpMen').show(); });
+  $('#popUpMen').click(function() { $('#popUpMen').hide(); });
+
+}
+
+
+
 //main method, sort of...
-
-console.log("hello");
-
-$(document).ready(init);
-
-var init = function()
-{
-
-  soundButton = $('#sound');
-  hiddenSound = $('#popUpSound');
-
-  // register all the event listeners
-  soundButton.on('click', showPopUp);
-  hiddenSound.on('click', hidePopUp);
-};
+//why does moving this from top to bottom help?
+$(document).on('ready',init);
 
 
-var showPopUp = function()
-{
-
-hiddenSound.show();
-}
-
-var hidePopUp = function()
-{
-  hiddenSound.hide();
-}
 /*
 function showPopUp() {
   var popup = document.getElementById('popUpSound');
